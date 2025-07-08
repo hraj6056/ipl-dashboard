f80ad2c321b0ce85c6b5bcbc1a2d90077b97d56f
Harsh <harsh.raj@314ecorp.com>
2025-07-08 19:27:09 +0530

    added readme file

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ import { NotificationBell } from "./NotificationBell";
 export const Navbar: React.FC = () => (
   <nav className="bg-indigo-600 text-white p-4 flex justify-between items-center">
     <Link href="/" className="text-lg font-semibold">
-      IPL T20 Dashboard
+      IPL Dashboard
     </Link>
     <div className="space-x-4 flex items-center">
       <Link href="/">
